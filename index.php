3f474da548351109f0a4fde38d67f0437d162ea0
gene7 <faison.eugene@student.greenriver.edu>
2024-04-04 13:37:52 -0400

    hompage added

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "
+<form>
+    <label>Your Name:</label>
+    <input type='text' id='user-name' name='user-name'></input>
+    <label>Cupcake Flavors:</label>
+    <input type='checkbox' name='flavor[]'>Tiramisu</input>
+    <input type='checkbox' name='flavor[]'>The Grasshopper</input>
+    <input type='checkbox' name='flavor[]'>Whiskey Maple Bacon</input>
+    <input type='checkbox' name='flavor[]'>Carrot Walnut</input>
+    <input type='checkbox' name='flavor[]'>Salted Caramel Cupcake</input>
+    <input type='checkbox' name='flavor[]'>Red Velvet</input>
+    <input type='checkbox' name='flavor[]'>Lemon Drop</input>
+    <button type='submit'>Order</button>
+
+</form>";
+
